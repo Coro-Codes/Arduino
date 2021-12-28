5d62df5b605d818d4ddf5690bfef45125b333558
Coro-Codes <96754956+Coro-Codes@users.noreply.github.com>
2021-12-27 17:21:15 -0800

    Add files via upload

---
+++ sketch_dec27a/sketch_dec27a.ino
@@ -0,0 +1,32 @@
+//Project #2 12/27/2021
+int d= 100;
+
+void setup() {
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+
+}
+
+void loop() {
+for (int a = 2;a<7;a++ )  
+{
+
+   digitalWrite(a, HIGH);
+delay(d);
+  digitalWrite(a, LOW);
+  delay(d);
+
+}
+
+for (int a=5; a>1; a--)
+{
+ digitalWrite(a, HIGH);
+delay(d);
+  digitalWrite(a, LOW);
+  delay(d);
+  
+}
+}
