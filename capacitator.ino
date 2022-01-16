392b2b5ccf44bd61bfca55c6b583f628e8c62ae1
Coro-Codes <96754956+Coro-Codes@users.noreply.github.com>
2022-01-16 13:03:54 -0800

    Create capacitator.ino

---
+++ capacitator.ino
@@ -0,0 +1,21 @@
+#define LED 3
+#define BUTTON 7
+
+
+void setup() {
+ pinMode(LED, OUTPUT);
+ pinMode(BUTTON, INPUT);
+}
+
+
+void loop() {
+if ( digitalRead(BUTTON) == HIGH )
+{digitalWrite(LED, HIGH);
+delay(500);
+digitalWrite(LED, LOW);
+delay(500);
+digitalWrite(LED, HIGH);
+delay(500);
+digitalWrite(LED, LOW);
+}
+}
